7d364b31aec8b218e201befb131a64b989d99eaf
myntora <koyluferdi@hotmail.com>
2025-06-15 23:43:19 +0300

    Updated site

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { FiMenu } from 'react-icons/fi';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">Myntora</div>
-      <ul className="navbar-links">
-        <li><Link to="/Home">Home</Link></li>
-        <li><Link to="/About">Information</Link></li>
-        <li><Link to="/games">Games</Link></li>
-        <li><Link to="/solutions">Solutions</Link></li>
+
+      <ul className={`navbar-links ${menuOpen ? 'open' : ''}`}>
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/games" onClick={closeMenu}>Games</Link></li>
+        <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+        <li><Link to="/solutions" onClick={closeMenu}>Solutions</Link></li>
       </ul>
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        <FiMenu />
+      </button>
+
     </nav>
   );
 };

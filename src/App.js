d89f94bd74e3535bee8f35d64a43cdc260d64134
myntora <koyluferdi@hotmail.com>
2025-06-15 22:48:29 +0300

    Updated site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import Games from './pages/Games';
 import Solutions from './pages/Solutions';
-
-import './App.css'; // Burada background tanımı yapılmış olacak
+import About from './pages/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/myntora-site">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/games" element={<Games />} />
         <Route path="/solutions" element={<Solutions />} />
-
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );

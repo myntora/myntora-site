b4b6fb03aed435c112e66a8719c6b36937105a57
myntora <koyluferdi@hotmail.com>
2025-09-13 13:34:11 +0300

    Fix BrowserRouter basename and add 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,18 +7,21 @@ import Games from './pages/Games';
 import Solutions from './pages/Solutions';
 import './App.css';
 
-const basename = new URL(process.env.PUBLIC_URL || '/', window.location.origin).pathname;
+
+const BASENAME =
+  process.env.NODE_ENV === 'production' ? '/myntora-site' : '/';
 
 function App() {
   return (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename={BASENAME}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/about" element={<About />} />
         <Route path="/games" element={<Games />} />
         <Route path="/solutions" element={<Solutions />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
